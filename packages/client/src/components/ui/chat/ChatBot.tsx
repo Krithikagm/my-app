5f826c4b7fbf834bb611f00b3dc332f9bf24a25c
Krithika Gandlur <krithikagandlur@gmail.com>
2025-11-13 16:30:18 -0600

    Exporting ChatInput

--- packages/client/src/components/ui/chat/ChatBot.tsx
+++ packages/client/src/components/ui/chat/ChatBot.tsx
@@ -1,56 +1,40 @@
-import { useForm } from 'react-hook-form';
-import { Button } from '../button';
-import { FaArrowUp } from 'react-icons/fa';
-import { useRef, useState, type KeyboardEvent } from 'react';
+import { useRef, useState } from 'react';
 import axios from 'axios';
-// import ReactMarkdown from 'react-markdown';
-import TypingIndicator from './TypingIndicator';
-import type { Message } from './ChatMessages';
-import ChatMessages from './ChatMessages';
 
-type FormData = {
-   prompt: string;
-};
+import ChatInput, { type ChatFormData } from './ChatInput';
+import ChatMessages, { type Message } from './ChatMessages';
+import TypingIndicator from './TypingIndicator';
 
 type ChatResponse = {
    message: string;
 };
 
-const chatBot = () => {
+export default function ChatBot() {
    const [messages, setMessages] = useState<Message[]>([]);
    const [isBotTyping, setIsBotTyping] = useState(false);
    const [error, setError] = useState('');
-   const conversationId = useRef(crypto.randomUUID());
+   const conversationId = useRef<string>(crypto.randomUUID());
 
-   const { register, handleSubmit, reset, formState } = useForm<FormData>();
-
-   const onSubmit = async ({ prompt }: FormData) => {
+   const onSubmit = async ({ prompt }: ChatFormData) => {
       try {
          setMessages((prev) => [...prev, { content: prompt, role: 'user' }]);
          setIsBotTyping(true);
          setError('');
-         reset({ prompt: '' });
+
          const { data } = await axios.post<ChatResponse>('/api/chat', {
             prompt,
             conversationId: conversationId.current,
          });
+
          setMessages((prev) => [
             ...prev,
             { content: data.message, role: 'Bot' },
          ]);
-         setIsBotTyping(false);
-      } catch (error) {
-         console.error(error);
-         setError('something went wrong try again!');
+      } catch (err) {
+         console.error(err);
+         setError('Something went wrong. Please try again.');
       } finally {
          setIsBotTyping(false);
-      }
-   };
-
-   const onKeyDown = (e: KeyboardEvent<HTMLTextAreaElement>) => {
-      if (e.key === 'Enter' && !e.shiftKey) {
-         e.preventDefault();
-         handleSubmit(onSubmit)();
       }
    };
 
@@ -59,33 +43,10 @@ const chatBot = () => {
          <div className="flex flex-col flex-1 gap-3 mb-10 overflow-y-auto">
             <ChatMessages messages={messages} />
             {isBotTyping && <TypingIndicator />}
-
             {error && <p className="text-red-500">{error}</p>}
          </div>
-         <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl"
-         >
-            <textarea
-               {...register('prompt', {
-                  required: true,
-                  validate: (data) => data.trim().length > 0,
-               })}
-               autoFocus
-               className="w-full border-0 focus:outline-0 resize-none "
-               placeholder="Ask Me anything"
-               maxLength={1000}
-               onKeyDown={onKeyDown}
-            />
-            <Button
-               disabled={!formState.isValid}
-               className="rounded-full w-9 h-9"
-            >
-               <FaArrowUp />
-            </Button>
-         </form>
+
+         <ChatInput onSubmit={onSubmit} />
       </div>
    );
-};
-
-export default chatBot;
+}
